4efc145bebddc2a72caf86f43c1717c79fb00578
tokyohandsome <34906599+tokyohandsome@users.noreply.github.com>
2024-01-09 00:47:09 +0900

    Special character set selectable
    Added radio buttons to select a set of special characters.

--- fletpassgen.py
+++ fletpassgen.py
@@ -12,10 +12,21 @@ def main(page: ft.Page):
         digits = string.digits
         punctuation = punctuation
         return(''.join(secrets.choice(punctuation + letters + digits) for i in range(len_pw)))
+
+    def change_sp_char(e):
+        sp_char.value = radio_sp_char.value
+        page.update()    
     
     pw_len = ft.TextField(value=default_pw_len, text_align=ft.TextAlign.RIGHT, width=100)
     pw_view = ft.TextField(value=password, text_align=ft.TextAlign.LEFT, width=300)
-    #password = generate_password(pw_len.value)
+    sp_char = ft.TextField(value=string.punctuation, text_align=ft.TextAlign.LEFT, width=300)
+    radio_sp_char = ft.RadioGroup(value=string.punctuation, content=ft.Row([
+            ft.Radio(value=string.punctuation, label="All"),
+            ft.Radio(value="!@$%^&*+#", label="Simple"),
+            ft.Radio(value="-_", label="URL safe"),
+            ft.Radio(value="none", label="None"), 
+            ]), on_change=change_sp_char
+            ) 
 
     def minus_click(e):
         pw_len.value = str(int(pw_len.value) - 1)
@@ -26,15 +37,20 @@ def main(page: ft.Page):
         page.update()
 
     def generate_password(e):
-        pw_view.value = passgen(int(pw_len.value))
+        if sp_char.value == "none":
+            pw_view.value = passgen(int(pw_len.value), '') 
+        else:
+            pw_view.value = passgen(int(pw_len.value), sp_char.value)
         page.update()
 
     def copy_password(e):
         page.set_clipboard(pw_view.value)
         page.update
-        
+
+
+
     page.add(
-        ft.Column(controls=[
+        ft.Column([
             ft.Row([
                 ft.Text(value="Num of characters: "),
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
@@ -47,8 +63,19 @@ def main(page: ft.Page):
             ft.Row(controls=[
                 ft.Text(value="Password: "),
                 pw_view,
-                ft.IconButton(ft.icons.COPY, on_click=copy_password)
+                ft.IconButton(ft.icons.COPY, tooltip="Copy", on_click=copy_password)
                 ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            ft.Row([
+                ft.Text("Special characters: "),
+                sp_char,
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            ft.Row(controls=[
+                radio_sp_char
+            ],
             alignment=ft.MainAxisAlignment.CENTER,
             )
         ]
